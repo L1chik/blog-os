3c310b83d1d39f9f9471f90398c5847223a351f9
Глеб Власов <vgg@MacBook-Air-Gleb.local>
2021-12-11 11:50:09 +0300

    vga_buffer progress

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,37 @@
 #![no_main]
 
 mod vga_buffer;
+
+// mod vga_buffer;
     use core::panic::PanicInfo;
 
     static HELLO: &[u8] = b"Hello World!";
 
     #[no_mangle]
     pub extern "C" fn _start() -> ! {
-        let vga_buffer = 0xb8000 as *mut u8;
+        use core::fmt::Write;
+        // let vga_buffer = 0xb8000 as *mut u8;
+        //
+        // // for (i, &byte) in HELLO.iter().enumerate() {
+        // //     unsafe  {
+        // //         *vga_buffer.offset(i as isize * 2) = byte;
+        // //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        // //     }
+        // // }
+        //
+        // "Hello, world!"
+        //     .as_bytes()
+        //     .iter()
+        //     .flat_map(|bt| [*bt, 0xc as u8])
+        //     .enumerate()
+        //     .for_each(|(i, byte)| unsafe {
+        //         *vga_buffer.offset(i as isize) = byte;
+        //     });
 
-        // for (i, &byte) in HELLO.iter().enumerate() {
-        //     unsafe  {
-        //         *vga_buffer.offset(i as isize * 2) = byte;
-        //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        //     }
-        // }
+        // vga_buffer::print_something();
 
-        "Hello, world!"
-            .as_bytes()
-            .iter()
-            .flat_map(|bt| [*bt, 0xc as u8])
-            .enumerate()
-            .for_each(|(i, byte)| unsafe {
-                *vga_buffer.offset(i as isize) = byte;
-            });
+        vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+        write!(vga_buffer::WRITER.lock(), " , some numbers: {} {}", 42, 1.337).unwrap();
 
         loop {}
     }
